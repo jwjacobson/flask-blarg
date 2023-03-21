e0c89051bd7b0366c5283d1e9db9fc0636ef9863
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-21 13:29:12 -0400

    Add login and signup pages

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,14 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
-@app.route('/test')
-def hello_test():
-    return 'This is not a test!'
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
